062d0e903ad4000565c9a41ec9f968ff10b32e53
David Kane <david@kane.sh>
2024-06-04 09:50:41 -0700

    Update aws-session-manager-plugin.rb
    Update version and hash to latest

--- Formula/aws-session-manager-plugin.rb
+++ Formula/aws-session-manager-plugin.rb
@@ -1,9 +1,9 @@
 class AwsSessionManagerPlugin < Formula
   desc "Official Amazon AWS session manager plugin"
   homepage "https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html"
-  url "https://s3.amazonaws.com/session-manager-downloads/plugin/1.2.312.0/mac/sessionmanager-bundle.zip"
+  url "https://s3.amazonaws.com/session-manager-downloads/plugin/1.2.633.0/mac/sessionmanager-bundle.zip"
 
-  sha256 "50aac34a4dedddf20c20be24989ee5d33b46a72187791715fb9b395b54db8ef9"
+  sha256 "f9fdff4bdc071083ef4b9f5e3ff1d9f6009c95bf13eb336a467ac487b37b282c"
 
   depends_on "awscli"
 
